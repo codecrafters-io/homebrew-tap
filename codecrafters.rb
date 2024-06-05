3ba2d3bd528f64c319360d153951668724b936cc
Paul Kuruvilla <paul@codecrafters.io>
2024-06-05 18:11:46 +0100

    Brew formula update for cli version v29

--- codecrafters.rb
+++ codecrafters.rb
@@ -5,21 +5,21 @@
 class Codecrafters < Formula
   desc "CodeCrafters CLI"
   homepage "https://codecrafters.io.io"
-  version "26"
+  version "29"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/codecrafters-io/cli/releases/download/v26/v26_darwin_amd64.tar.gz"
-      sha256 "c833e38e56c4e372e2cd1d8f31c7b841dd56fdd2171d41b465fc3e1a5c970291"
+      url "https://github.com/codecrafters-io/cli/releases/download/v29/v29_darwin_amd64.tar.gz"
+      sha256 "50332ac96e9b12b5c4c07b7fbcef6edd8e848c9ee8cc1e7cea82f5330f0737d3"
 
       def install
         bin.install "codecrafters"
       end
     end
     on_arm do
-      url "https://github.com/codecrafters-io/cli/releases/download/v26/v26_darwin_arm64.tar.gz"
-      sha256 "d9396317a08df008689291b3c1cd7f72ad159fc8eeeb0f8da36fd936374ec4f8"
+      url "https://github.com/codecrafters-io/cli/releases/download/v29/v29_darwin_arm64.tar.gz"
+      sha256 "77194282fa91fd309e2638db963225a9aacf774d57c32ee7d03d1950df86cb17"
 
       def install
         bin.install "codecrafters"
@@ -30,8 +30,8 @@ class Codecrafters < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/codecrafters-io/cli/releases/download/v26/v26_linux_amd64.tar.gz"
-        sha256 "92e9e15beb897e94d0a48147d760fb82755300a56334b124100183151e985e0d"
+        url "https://github.com/codecrafters-io/cli/releases/download/v29/v29_linux_amd64.tar.gz"
+        sha256 "589136a38b6fabd5b37d62185d727680815fd3b17947b096881a817e7364b80a"
 
         def install
           bin.install "codecrafters"
@@ -40,8 +40,8 @@ class Codecrafters < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/codecrafters-io/cli/releases/download/v26/v26_linux_arm64.tar.gz"
-        sha256 "c3c7ac4f2138f45bfea46c5242d7d9a7f3c3185104504f691efc66d5ebfd7100"
+        url "https://github.com/codecrafters-io/cli/releases/download/v29/v29_linux_arm64.tar.gz"
+        sha256 "1b36f73b6349864d356fa4e6c26efdcfa7bbef1287240f32a02b30b1cd1a022d"
 
         def install
           bin.install "codecrafters"
